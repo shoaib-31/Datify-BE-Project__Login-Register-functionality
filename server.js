e7b312a86a7769871f4e59348cfd219fb812def6
Shoaib Akhtar <shoaibakmasood@gmail.com>
2023-11-30 13:37:58 +0530

    Added README.md

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ sequelize
   .sync()
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log("Server is running on port 3000");
+      console.log(`Server is running on port ${process.env.PORT}}`);
     });
   })
   .catch((error) => {
